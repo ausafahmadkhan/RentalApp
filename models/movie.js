f0222ea9212a635f62072d619ea53941b40a0ffc
Ausaf Khan <khan.aakhan.ausaf7@gmail.com>
2019-12-02 02:09:01 +0530

    Secondary (#4)
    * Added Movies

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
-const { genreSchema } = require('./genre')
+const { genreSchema } = require('./genre');
 
 Joi.objectId = require('joi-objectid')(Joi);
 
@@ -11,27 +11,33 @@ const movieSchema = new mongoose.Schema(
             type : String,
             required : true,
             minlength : 5,
-            maxlength : 10,
+            maxlength : 255,
             trim : true
         },
         genre : 
         {
-            type :genreSchema,
+            type : genreSchema,
             required : true
         },
         inStockNumber : 
         {
             type : Number,
             required : true,
-            min : 5,
-            max : 100
+            min : 0,
+            max : 255
         },
         dailyRentalRate :
         {
             type : Number,
             required : true,
-            min : 10,
-            max : 50
+            min : 0,
+            max : 255
+        },
+        datePublished :
+        {
+            type : Date,
+            default : Date.now(),
+            required : false
         }
 
     }
@@ -42,14 +48,14 @@ const Movie = mongoose.model('movies', movieSchema);
 function validateMovie(movie)
 {
     const schema = {
-        title : Joi.string().min(5).max(10).required(),
+        title : Joi.string().min(5).max(255).required(),
         genreId : Joi.objectId().required(),
-        inStockNumber : Joi.number().min(5).max(100).required(),
-        dailyRentalRate : Joi.number().min(10).max(50).required()
+        inStockNumber : Joi.number().min(0).max(255).required(),
+        dailyRentalRate : Joi.number().min(0).max(255).required()
     }
 
-    return Joi.validate(movie, movieSchema);
+    return Joi.validate(movie, schema);
 }
 
 module.exports.Movie = Movie;
-module.exports.validate= validateMovie;
+module.exports.validate = validateMovie;
---
+++ router/movie_router.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre');
+
+router.use(express.json());
+
+router.get("/getMovies", async (req, res) => {
+    const movies = await Movie.find()
+    .select("-__v")
+    .sort("datePublished");
+    
+    return res.status(200).send(movies);
+})
+
+router.post("/addMovie", async (req, res) => {
+    const { error } = validate(req.body);
+
+    if (error)
+        return res.status(400).send(`Invalid Movie : ${error.details[0].message}`);
+
+    const genre = await Genre.findById(req.body.genreId);
+
+    if (!genre)
+        return res.status(404).send(`Invalid genre Id`);
+
+    let movie = new Movie(
+        {
+            title : req.body.title,
+            genre : {
+                _id : genre._id,
+                name : genre.name
+            },
+            inStockNumber : req.body.inStockNumber,
+            dailyRentalRate : req.body.dailyRentalRate
+        }
+    )
+
+    movie = await movie.save();
+
+    return res.status(200).send(movie);
+})
+
+router.get("/getMovie/:id", async (req, res) => {
+    const movie = await Movie.findById(req.params.id).select("-__v");
+    
+    if (!movie)
+    {
+        return res.status(404).send("No movie present with the given id");
+    }
+
+    return res.status(200).send(movie);
+})
+
+router.put("/updateMovie/:id", async (req, res) => {
+
+    const { error } = await validate(req.body);
+
+    if (error)
+        return res.status(400).send(`Invalid movie : ${error.details[0].message}`);
+
+    const genre = await Genre.findById(req.body.genreId);
+
+    if (!genre)
+        return res.status(404).send(`Invalid Genre Id`);
+
+    const movie =  await Movie.findByIdAndUpdate(req.params.id, 
+        {
+            title : req.body.title,
+            genre : {
+                _id : genre._id,
+                name : genre.name
+            },
+            inStockNumber : req.body.inStockNumber,
+            dailyRentalRate : req.body.dailyRentalRate
+        },
+        {
+            new : true
+        }
+    );
+
+    if (!movie)
+        return res.status(404).send("No movie present with the given id");
+
+    return res.status(200).send(movie);
+
+})
+
+router.delete("/deleteMovie/:id", async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id).select("-__v");
+
+    if (!movie)
+        return res.status(404).send("No movie with the given id");
+
+    return res.status(200).send(movie);
+})
+
+module.exports = router;
