5a7843d758f3b75153a0e9f4c56733ae73a6b3a8
Ausaf Khan <khan.aakhan.ausaf7@gmail.com>
2019-12-05 16:26:03 +0530

    Secondary (#7)
    * Added custom async validator

--- models/customer.js
+++ models/customer.js
@@ -9,6 +9,7 @@ const customerSchema = new mongoose.Schema(
             required : true,
             minlength : 4,
             maxlength : 20,
+            // match : /pattern/
         },
         isGold :
         {
@@ -21,10 +22,43 @@ const customerSchema = new mongoose.Schema(
             required : true,
             minlength : 5,
             maxlength : 15
+        },
+        address :
+        {
+            type : Array,
+            validate : 
+            {
+                isAsync : true,
+                validator :  function(address, callback) {
+                    if (Array.isArray(address))
+                    {
+                        if (address.length === 0)
+                            callback(false);
+
+                        address.forEach(function(a)
+                        {
+                            if (typeof a !== 'string')
+                            {
+                                callback(false);
+                            }
+                            else
+                            {
+                                a = a.trim();
+                                if (a === "")
+                                {
+                                    callback(false);
+                                }
+                            }
+                        })
+                        callback(true);
+                    }
+                    callback(false);
+                },
+                message : "Should have atleast one address and has to be a non-empty string."
+            },
         }
-        //and several other fields
-    }
-);
+        //and several other fields        
+    })
 
 const Customer = mongoose.model('Customer', customerSchema);
 
@@ -33,7 +67,8 @@ function validateCustomer(customer)
     const schema = {
         name : Joi.string().min(4).max(50).required(),
         phoneNumber : Joi.string().min(5).max(15).required(),
-        isGold : Joi.boolean()
+        isGold : Joi.boolean(),
+        address : Joi.array().required()
     }
     return Joi.validate(customer, schema);
 }
