0e5db4079d23c0f9036d761173474805b5621ab7
Ausaf Khan <khan.aakhan.ausaf7@gmail.com>
2019-12-13 02:58:12 +0530

    Added user registration

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const customerRouter = require('./router/customer_router');
 const genreRouter = require('./router/genre_router');
 const movieRouter = require('./router/movie_router');
 const rentalRouter = require('./router/rental_router');
+const userRouter = require('./router/user_router');
+const config = require('config');
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/playground",
@@ -14,11 +16,17 @@ mongoose.connect("mongodb://localhost:27017/playground",
                 .then(() => console.log("Connected to db successfully"))
                 .catch((err) => console.log(`Could not connect to db due to ${err}`));
         
+if (!config.get('mySecretKey'))
+{
+    console.error("Jwt key not defined...exiting")
+    process.exit(0);
+}
 
 app.use("/customer", customerRouter);
 app.use("/genre", genreRouter);
 app.use("/movie", movieRouter);
 app.use("/rental", rentalRouter);
+app.use("/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
